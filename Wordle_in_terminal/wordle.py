a940f8ba99dabe69911f0eb9f7d1054e8247e518
ValMar <markov306@gmail.com>
2022-03-28 09:42:40 +0300

    update wordle project

--- Wordle_in_terminal/wordle.py
+++ Wordle_in_terminal/wordle.py
@@ -5,12 +5,22 @@ CHOSEN_WORD = random.choice(valid_words)
 GUESSES_COUNT = 7
 
 
+class Color:
+    PREFIX = '\033'
+    BASE = '\033[0m'
+    GREY = '\033[90m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+
+
 class GuessWord:
     counter = 1
 
     def __init__(self, w_str: str):
         self.w_str = w_str
         self.w_chars = list(self.w_str)
+        self.post_guess_w_str = ""
 
     def jump_turn(self):
         GuessWord.counter += 1
@@ -18,3 +28,15 @@ class GuessWord:
     def is_valid(self):
         return self.w_str in valid_words
 
+    def apply_greens(self):
+        for i, _ in enumerate(self.w_chars):  # we use only i parameter
+            actual_char = CHOSEN_WORD[i]
+            guessed_char = self.w_chars[i]
+            if actual_char == guessed_char:
+                colored_char = f'{Color.GREEN}{actual_char}{Color.BASE}'
+                self.w_chars[i] = colored_char
+
+    def apply_guesses(self):
+        self.apply_greens()
+        self.post_guess_w_str = ''.join(self.w_chars)
+        print(self.post_guess_w_str)
