934c6c230524f06105950827ccbffa3400b0653d
Valentin Markov <markov306@gmail.com>
2022-11-14 19:47:23 +0200

    add guessing char function

--- Hangman/hangman_utils.py
+++ Hangman/hangman_utils.py
@@ -13,7 +13,34 @@ def display_word(word: str):
     """Print random word for player"""
     first = word[0:1]
     last = word[len(word) - 1:len(word)]
-    return first, last
+    middle_part = (word.lstrip(first)).rstrip(last)
+    new_word = ''
+    for chafr in middle_part:
+        if chafr.isalpha():
+            new_word = new_word + ' _ '
+    return f'{first}{new_word}{last}'
 
 
-print(display_word(pick_up_word(words)))
+def middle_part(word: str):
+    first = word[0:1]
+    last = word[len(word) - 1:len(word)]
+    mid_part = (word.lstrip(first)).rstrip(last)
+
+    return mid_part
+
+
+def guessing_char(word: str):
+    text = ''
+    for char in word:
+        if char.isalpha():
+            text = text + ' _ '
+
+    char = input('Please enter char: ')
+    if char in word:
+
+
+        pass
+
+
+# print(display_word(pick_up_word(words)))
+print(middle_part(pick_up_word(words)))
