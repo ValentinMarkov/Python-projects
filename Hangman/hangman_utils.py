9dc5f491546e780468a22f73ca7d26d95a7b53c1
Valentin Markov <markov306@gmail.com>
2022-11-19 08:03:09 +0200

    add main file and refactoring game func

--- Hangman/hangman_utils.py
+++ Hangman/hangman_utils.py
@@ -14,97 +14,81 @@ def create_word_parts(word: str):
     first = word[0:1]
     last = word[len(word) - 1:len(word)]
     mid_part = (word.lstrip(first)).rstrip(last)
-    first_last = word.replace(mid_part, ' _ ' * len(mid_part))
+    first_last = word.replace(mid_part, '*' * len(mid_part))
 
     return first_last, mid_part
 
 
 def player_input():
+    """Receive player letter"""
     player_letter = input('Please enter letter: ')
 
     return player_letter
+
+
+def play_again():
+    player_choice = input("Play again? (y/n):\n")
+    if player_choice == 'y':
+        game()
+    else:
+        pass
 
 
 def game():
     main_word = pick_up_word()
     word_parts = create_word_parts(main_word)
 
-    first_last_letter = word_parts[0]  # l _  _  _  _ p
-    mid_letters = word_parts[1]  # apto
+    first_last_letter = word_parts[0]
+    mid_letters = word_parts[1]
+
+    # Replace mid letters with symbols
     shadow = '*' * len(mid_letters)
 
-    while True:
-        print(first_last_letter)
-        my_dict = {}  # {0: 'h', 1: 'a', 2: 'i'}
-        lst_shadow = list(shadow)
+    # Create lists with letters and symbols
+    lst_shadow = list(shadow)
+    lst_first_last_letter = list(first_last_letter)
+
+    playing = True
+    wrong_letter_cnt = 0
+
+    while playing:
+        print('# '*20)
+        display_word = ''
+        for char in lst_first_last_letter:
+            display_word = display_word + char
+
+        print(f"Guess the word:\n{display_word}")
+
+        # Dict contains mid letters
+        my_dict = {}
 
         for count, value in enumerate(mid_letters):
             my_dict[count] = value
 
-        lst_keys = list(my_dict.keys())
         lst_values = list(my_dict.values())
 
         player_char = player_input()
 
         if player_char in lst_values:
+            print(f"You guessed the letter '{player_char}'")
             char_index = lst_values.index(player_char)
             char = lst_values[char_index]
             lst_shadow[char_index] = char
-            print(lst_shadow)
+            lst_first_last_letter[char_index + 1] = char
+
         else:
-            print("Wrong letter")
+            print(f"Wrong letter '{player_char}'. Try Again!")
+            wrong_letter_cnt += 1
+
+            if wrong_letter_cnt > 2:
+                print('You are hanged!!! Game over.')
+                # TODO: Create property code here
 
         if "*" not in lst_shadow:
+            print(f'You won! The word is: {main_word.upper()}')
+
+            play_again()
+            playing = False
             break
 
 
-game()
-
-# def word_after_correct_guessed_char(word: str, char):
-#     display_w = ''
-#     for i in word:
-#         if i == char:
-#             display_w = f"{display_w} {char} "
-#         else:
-#             display_w = display_w + ' _ '
-#
-#     return display_w
-#
-#
-# def guessing_char(word: str):
-#     print(word)
-#     text = ''
-#     for char in word:
-#         if char.isalpha():
-#             text = text + ' _ '
-#
-#     # This dic contain word middle part
-#     mid_part = middle_part(word)
-#     my_dict = {}  # {0: 'h', 1: 'a', 2: 'i'}
-#
-#     print(f'Mid part: {mid_part}')
-#     for count, value in enumerate(mid_part):
-#         my_dict[count] = value
-#
-#     print(my_dict)
-#     char = input('Please enter char: ')
-#
-#     lst_key = list(my_dict.keys())
-#     lst_values = list(my_dict.values())
-#
-#     guessed_letter = ''
-#
-#     if char in lst_values:
-#         c_index = lst_values.index(char)
-#         guessed_letter = lst_key.index(c_index)
-#         print(word_after_correct_guessed_char(word, guessed_letter))
-#
-#     else:
-#         print('Wrong char')
-#
-#
-#
-#
-# # print(display_word(pick_up_word(words)))
-# # print(middle_part(pick_up_word(words)))
-# guessing_char(pick_up_word(words))
---
+++ Hangman/main.py
@@ -0,0 +1,5 @@
+from hangman_utils import game
+
+
+if __name__ == '__main__':
+    game()
