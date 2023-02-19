2b4a00926b5dec1c0c64b36d3e5fb1df7109d162
ValMar <markov306@gmail.com>
2023-02-19 14:03:31 +0200

    update lower_higher game and start day 15 project

---
+++ UDEMY_courses/100_days_of_code/day_14_higher_lower_game_project/game_data.py
@@ -0,0 +1,18 @@
+lst_questions = [
+    {'Red hot chili paper': 250000},
+    {'Everest': 550000},
+    {'Japan': 1000000},
+    {'Paris': 703000},
+    {'London': 450000},
+    {'Scarlet Johanson': 389000},
+    {'Pinko': 90000},
+    {'Alf': 344000},
+    {'Bulgaria': 150000},
+    {'Zombie': 690000},
+    {'Friends': 2790000},
+    {'Germany': 490000},
+    {'Russia': 990000},
+
+
+
+]
